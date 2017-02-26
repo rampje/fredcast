2c582061934705e657364971620f8cffc329de0e
Warner <rampy@vt.edu>
2017-02-26 18:33:54 -0500

    "model widgets" up

--- server.R
+++ server.R
@@ -3,6 +3,7 @@ library(shinydashboard)
 library(forecast)
 library(plotly)
 library(fredr)
+library(DT)
 
 
 fredr_key("0e510a57a0086df706ac9c8fb852b706")
@@ -14,57 +15,112 @@ shinyServer(function(input, output){
   
   
   
-  output$plot.unemp <- renderPlotly({
+  output$plot.unemp <- renderPlot({
     unemp.model <- Arima(fredr_series(series_id = "UNRATE"), order = c(1,1,1))
     
     unemp.fcst <- forecast(unemp.model)
     
-    ggplotly(autoplot(unemp.fcst) + ylab("Unemployment Rate") + 
-               ggtitle(" ") + xlim(2012,2018))
+    autoplot(unemp.fcst) + ylab("Unemployment Rate") + ggtitle(" ")
   })
   
   output$unempSummary <- renderPrint({
     Arima(fredr_series(series_id = "UNRATE"), order = c(1,1,1))
   })
   
+  output$unempForecastTable <- renderDataTable({
+    unemp.model <- Arima(fredr_series(series_id = "UNRATE"), order = c(1,1,1))
+    
+    unemp.fcst <- forecast(unemp.model)
+    unemp.fcst <- data.frame(unemp.fcst)
+    
+    unemp.fcst.dates <- row.names(unemp.fcst)
+    
+    unemp.fcst <- data.frame(sapply(unemp.fcst, function(x) round(x, 2)))
+    unemp.fcst$Date <- unemp.fcst.dates
+    
+    datatable(head(unemp.fcst), rownames = FALSE)
+  })
   
-  output$plot.payemp <- renderPlotly({
+  
+  output$plot.payemp <- renderPlot({
     payemp.model <- Arima(fredr_series(series_id = "PAYEMS"), order=c(1,1,1))
     
     payemp.fcst <- forecast(payemp.model)
     
-    ggplotly(autoplot(payemp.fcst) + ylab("Payroll Employment") +
-              ggtitle(" ") + xlim(2008, 2018))
+    autoplot(payemp.fcst) + ylab("Payroll Employment") +
+            ggtitle(" ") + xlim(1990, 2018) + ylim(105000, 155000)
   })
   
   output$payempSummary <- renderPrint({
     Arima(fredr_series(series_id = "PAYEMS"), order=c(1,1,1))
   })
   
-  output$plot.gdp <- renderPlotly({
+  output$payempForecastTable <- renderDataTable({
+    payemp.model <- Arima(fredr_series(series_id = "PAYEMS"), order = c(1,1,1))
+    
+    payemp.fcst <- forecast(payemp.model)
+    payemp.fcst <- data.frame(payemp.fcst)
+    
+    payemp.fcst.dates <- row.names(payemp.fcst)
+    
+    payemp.fcst <- data.frame(sapply(payemp.fcst, function(x) round(x)))
+    payemp.fcst$Date <- payemp.fcst.dates
+    
+    datatable(head(payemp.fcst), rownames = FALSE)
+  })
+  
+  output$plot.gdp <- renderPlot({
     gdp.model <- Arima(fredr_series(series_id = "A191RL1Q225SBEA"), order = c(2,1,1))
     
     gdp.fcst <- forecast(gdp.model)
     
-    ggplotly(autoplot(gdp.fcst) + ylab("Real GDP Growth Rate") +
-               ggtitle(" ") + xlim(2012, 2018))
+    autoplot(gdp.fcst) + ylab("Real GDP Growth Rate") +
+               ggtitle(" ") + xlim(1990, 2018)
   })
   
   output$gdpSummary <- renderPrint({
     Arima(fredr_series(series_id = "A191RL1Q225SBEA"), order=c(1,1,1))
   })
   
-  output$plot.cpi <- renderPlotly({
+  output$gdpForecastTable <- renderDataTable({
+    gdp.model <- Arima(fredr_series(series_id = "A191RL1Q225SBEA"), order = c(1,1,1))
+    
+    gdp.fcst <- forecast(gdp.model)
+    gdp.fcst <- data.frame(gdp.fcst)
+    
+    gdp.fcst.dates <- row.names(gdp.fcst)
+    
+    gdp.fcst <- data.frame(sapply(gdp.fcst, function(x) round(x, 2)))
+    gdp.fcst$Date <- gdp.fcst.dates
+    
+    datatable(head(gdp.fcst), rownames = FALSE)
+  })
+  
+  output$plot.cpi <- renderPlot({
     cpi.model <- Arima(fredr_series(series_id = "CPIAUCSL"), order = c(2,1,1))
     
     cpi.fcst <- forecast(cpi.model)
     
-    ggplotly(autoplot(cpi.fcst) + ylab("CPI Growth Rate") +
-               ggtitle(" ") + xlim(2012, 2018))
+    autoplot(cpi.fcst) + ylab("CPI Growth Rate") +
+               ggtitle(" ") + xlim(1990, 2018) + ylim(125, 250)
   })
   
   output$cpiSummary <- renderPrint({
     Arima(fredr_series(series_id = "CPIAUCSL"), order=c(1,1,1))
+  })
+  
+  output$cpiForecastTable <- renderDataTable({
+    cpi.model <- Arima(fredr_series(series_id = "CPIAUCSL"), order = c(1,1,1))
+    
+    cpi.fcst <- forecast(cpi.model)
+    cpi.fcst <- data.frame(cpi.fcst)
+    
+    cpi.fcst.dates <- row.names(cpi.fcst)
+    
+    cpi.fcst <- data.frame(sapply(cpi.fcst, function(x) round(x, 1)))
+    cpi.fcst$Date <- cpi.fcst.dates
+    
+    datatable(head(cpi.fcst), rownames = FALSE)
   })
   
   
--- ui.R
+++ ui.R
@@ -3,10 +3,13 @@ library(shinydashboard)
 library(forecast)
 library(plotly)
 library(fredr)
+library(DT)
 
 unemp.model <- Arima(fredr_series(series_id = "UNRATE"), order = c(1,1,1))
 unemp.fcst <- forecast(unemp.model)
 unrate <- round(unemp.fcst$mean[2], 1) 
+
+datatable(data.frame(unemp.fcst))
 
 shinyUI(
   dashboardPage(
@@ -20,28 +23,36 @@ shinyUI(
     dashboardBody(
       
       tabBox(title = "Unemployment Forecast",
-             tabPanel("Forecast Graph", plotlyOutput("plot.unemp"),
+             tabPanel("Forecast Graph", plotOutput("plot.unemp"),
                       color = "light-blue"),
              tabPanel("Model Summary",
-                      verbatimTextOutput("unempSummary"))),
+                      verbatimTextOutput("unempSummary")),
+             tabPanel("Forecast Table",
+                      dataTableOutput("unempForecastTable"))),
       
       tabBox(title = "Monthly Payroll Employment",
-             tabPanel("Forecast Graph", plotlyOutput("plot.payemp"),
+             tabPanel("Forecast Graph", plotOutput("plot.payemp"),
                       color = "light-blue"),
              tabPanel("Model Summary",
-                      verbatimTextOutput("payempSummary"))),
+                      verbatimTextOutput("payempSummary")),
+             tabPanel("Forecast Table",
+                      dataTableOutput("payempForecastTable"))),
       
       tabBox(title = "Quarterly GDP Growth Rate",
-             tabPanel("Forecast Graph", plotlyOutput("plot.gdp"),
+             tabPanel("Forecast Graph", plotOutput("plot.gdp"),
                       color = "light-blue"),
              tabPanel("Model Summary",
-                      verbatimTextOutput("gdpSummary"))),
+                      verbatimTextOutput("gdpSummary")),
+             tabPanel("Forecast Table",
+                      dataTableOutput("gdpForecastTable"))),
       
       tabBox(title = "Consumer Price Index",
-             tabPanel("Forecast Graph", plotlyOutput("plot.cpi"),
+             tabPanel("Forecast Graph", plotOutput("plot.cpi"),
                       color = "light-blue"),
              tabPanel("Model Summary",
-                      verbatimTextOutput("cpiSummary")))
+                      verbatimTextOutput("cpiSummary")),
+             tabPanel("Forecast Table",
+                      dataTableOutput("cpiForecastTable")))
       )
     )
   )
