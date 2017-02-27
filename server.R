4c5b7561ae1587c995d50305f108064fd452237a
Warner <rampy@vt.edu>
2017-02-26 19:21:20 -0500

    'plotModel' function added

--- server.R
+++ server.R
@@ -16,25 +16,37 @@ fitModel <- function(seriesID){
     Arima(fredr_series(series_id = "CPIAUCSL"), order = c(2,1,1))
   }
 }
+plotModel <- function(model){
+  
+  fcst <- forecast(model)
+  
+  # assumes object is forecast model
+  if(grepl("PAYEMS", model$series)){
+    
+    autoplot(fcst) + ylab("Payroll Employment") +
+      ggtitle(" ") + xlim(1990, 2018) + ylim(105000, 155000)
+    
+  } else if(grepl("A191RL1Q225SBEA", model$series)){
+    
+    autoplot(fcst) + ylab("Real GDP Growth Rate") +
+      ggtitle(" ") + xlim(1990, 2018)
+    
+  } else if(grepl("CPIAUCSL", model$series)){
 
+    autoplot(fcst) + ylab("CPI Growth Rate") +
+      ggtitle(" ") + xlim(1990, 2018) + ylim(125, 250)
+  } else if(grepl("UNRATE", model$series)){
+    autoplot(fcst) + ylab("Unemployment Rate") + ggtitle(" ")
+  }
+}
 
 fredr_key("0e510a57a0086df706ac9c8fb852b706")
-#gdp <- 
-#payroll <- fredr_series(series_id = "PAYEMS")
-#cpi <- fredr_series(series_id = "CPIAUCSL")
 
 shinyServer(function(input, output){
   
-  output$plot.unemp <- renderPlot({
-    unemp.model <- fitModel("UNRATE")
-    
-    unemp.fcst <- forecast(unemp.model)
-    
-    autoplot(unemp.fcst) + ylab("Unemployment Rate") + ggtitle(" ")
-  })
-  output$unempSummary <- renderPrint({
-    fitModel("UNRATE")
-  })
+  # unemployment widget
+  output$plot.unemp <- renderPlot("UNRATE" %>% fitModel %>% plotModel)
+  output$unempSummary <- renderPrint(fitModel("UNRATE"))
   output$unempForecastTable <- renderDataTable({
     unemp.model <- fitModel("UNRATE")
     
@@ -49,18 +61,9 @@ shinyServer(function(input, output){
     datatable(head(unemp.fcst), rownames = FALSE)
   })
   
-  
-  output$plot.payemp <- renderPlot({
-    payemp.model <- Arima(fredr_series(series_id = "PAYEMS"), order=c(1,1,1))
-    
-    payemp.fcst <- forecast(payemp.model)
-    
-    autoplot(payemp.fcst) + ylab("Payroll Employment") +
-            ggtitle(" ") + xlim(1990, 2018) + ylim(105000, 155000)
-  })
-  output$payempSummary <- renderPrint({
-    Arima(fredr_series(series_id = "PAYEMS"), order=c(1,1,1))
-  })
+  # payroll employment widget
+  output$plot.payemp <- renderPlot("PAYEMS" %>% fitModel %>% plotModel)
+  output$payempSummary <- renderPrint(fitModel("PAYEMS"))
   output$payempForecastTable <- renderDataTable({
     payemp.model <- Arima(fredr_series(series_id = "PAYEMS"), order = c(1,1,1))
     
@@ -75,19 +78,9 @@ shinyServer(function(input, output){
     datatable(head(payemp.fcst), rownames = FALSE)
   })
   
-  output$plot.gdp <- renderPlot({
-    gdp.model <- Arima(fredr_series(series_id = "A191RL1Q225SBEA"), order = c(2,1,1))
-    
-    gdp.fcst <- forecast(gdp.model)
-    
-    autoplot(gdp.fcst) + ylab("Real GDP Growth Rate") +
-               ggtitle(" ") + xlim(1990, 2018)
-  })
-  
-  output$gdpSummary <- renderPrint({
-    Arima(fredr_series(series_id = "A191RL1Q225SBEA"), order=c(1,1,1))
-  })
-  
+  # gdp widget
+  output$plot.gdp <- renderPlot("A191RL1Q225SBEA" %>% fitModel %>% plotModel)
+  output$gdpSummary <- renderPrint(fitModel("A191RL1Q225SBEA"))
   output$gdpForecastTable <- renderDataTable({
     gdp.model <- Arima(fredr_series(series_id = "A191RL1Q225SBEA"), order = c(1,1,1))
     
@@ -102,19 +95,9 @@ shinyServer(function(input, output){
     datatable(head(gdp.fcst), rownames = FALSE)
   })
   
-  output$plot.cpi <- renderPlot({
-    cpi.model <- Arima(fredr_series(series_id = "CPIAUCSL"), order = c(2,1,1))
-    
-    cpi.fcst <- forecast(cpi.model)
-    
-    autoplot(cpi.fcst) + ylab("CPI Growth Rate") +
-               ggtitle(" ") + xlim(1990, 2018) + ylim(125, 250)
-  })
-  
-  output$cpiSummary <- renderPrint({
-    Arima(fredr_series(series_id = "CPIAUCSL"), order=c(1,1,1))
-  })
-  
+  # cpi widget
+  output$plot.cpi <- renderPlot("CPIAUCSL" %>% fitModel %>% plotModel)
+  output$cpiSummary <- renderPrint(fitModel("CPIAUCSL"))
   output$cpiForecastTable <- renderDataTable({
     cpi.model <- Arima(fredr_series(series_id = "CPIAUCSL"), order = c(1,1,1))
     
@@ -128,6 +111,4 @@ shinyServer(function(input, output){
     
     datatable(head(cpi.fcst), rownames = FALSE)
   })
-  
-  
 })
