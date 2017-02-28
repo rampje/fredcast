e9cdee1108bc128286d494141c5b7267abcf33c2
Warner <rampy@vt.edu>
2017-02-27 22:28:20 -0500

    server.R now reads models into memory

--- server.R
+++ server.R
@@ -2,21 +2,29 @@ source("functions.R")
 
 fredr_key(key)
 
+unemp.mod <- readRDS("unrate-AutoArima.rds")
+payemp.mod <- readRDS("payems-AutoArima.rds")
+gdp.mod <- readRDS("gdp-AutoArima.rds")
+cpi.mod <- readRDS("cpi-AutoArima.rds")
+
 shinyServer(function(input, output){
   # unemployment widget
-  output$plot.unemp <- renderPlot("UNRATE" %>% fitModel %>% plotModel)
-  output$unempSummary <- renderPrint("UNRATE" %>% fitModel)
-  output$unempForecastTable <- renderDataTable("UNRATE" %>% fitModel %>% forecastTable)
+  output$plot.unemp <- renderPlot(unemp.mod %>% plotModel)
+  output$unempSummary <- renderPrint(unemp.mod)
+  output$unempForecastTable <- renderDataTable(unemp.mod %>% forecastTable)
   # payroll employment widget
-  output$plot.payemp <- renderPlot("PAYEMS" %>% fitModel %>% plotModel)
-  output$payempSummary <- renderPrint(fitModel("PAYEMS"))
-  output$payempForecastTable <- renderDataTable("PAYEMS" %>% fitModel %>% forecastTable)
+  output$plot.payemp <- renderPlot(payemp.mod %>% plotModel)
+  output$payempSummary <- renderPrint(payemp.mod)
+  output$payempForecastTable <- renderDataTable(payemp.mod %>% forecastTable)
   # gdp widget
-  output$plot.gdp <- renderPlot("A191RL1Q225SBEA" %>% fitModel %>% plotModel)
-  output$gdpSummary <- renderPrint("A191RL1Q225SBEA" %>% fitModel)
-  output$gdpForecastTable <- renderDataTable("A191RL1Q225SBEA"%>%fitModel%>%forecastTable)
+  output$plot.gdp <- renderPlot(gdp.mod %>% plotModel)
+  output$gdpSummary <- renderPrint(gdp.mod)
+  output$gdpForecastTable <- renderDataTable(cpi.mod %>% forecastTable)
   # cpi widget
-  output$plot.cpi <- renderPlot("CPIAUCSL" %>% fitModel %>% plotModel)
-  output$cpiSummary <- renderPrint("CPIAUCSL" %>% fitModel)
-  output$cpiForecastTable <- renderDataTable("CPIAUCSL" %>% fitModel %>% forecastTable)
+  output$plot.cpi <- renderPlot(cpi.mod %>% plotModel)
+  output$cpiSummary <- renderPrint(cpi.mod)
+  output$cpiForecastTable <- renderDataTable(cpi.mod %>% forecastTable)
 })
+
+
+
