3a5b3793fb99097f4d5deb7ee7e0751420d2e986
Warner <rampy@vt.edu>
2017-02-27 22:27:51 -0500

    saves auto.arima models as rds files

--- eda, model - UNRATE.R
+++ eda, model - UNRATE.R
@@ -1,10 +1,16 @@
 library(forecast)
 library(fredr)
 
+source("functions.R")
+
 unrate <- fredr_series(series_id = "UNRATE")
 
-acf(unrate)
-pacf(unrate)
+unr.mod <- fitModel("UNRATE")
+pay.mod <- fitModel("PAYEMS")
+gdp.mod <- fitModel("A191RL1Q225SBEA")
+cpi.mod <- fitModel("CPIAUCSL")
 
-acf(diff(unrate))
-pacf(diff(unrate))
+saveRDS(unr.mod, file = "unrate-AutoArima.rds")
+saveRDS(pay.mod, file = "payems-AutoArima.rds")
+saveRDS(gdp.mod, file = "gdp-AutoArima.rds")
+saveRDS(cpi.mod, file = "cpi-AutoArima.rds")
